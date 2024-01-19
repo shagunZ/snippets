0848a19f915d06003e7b344feb8c49a8c370fb5b
Shagun Mengi <shagunmengi003@gmail.com>
2024-01-19 23:53:00 +0530

    image-snippets

---
+++ components/CodeEditor.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from 'react'
+import { Resizable } from 're-resizable';
+import AceEditor from "react-ace";
+//themes
+import "ace-builds/src-noconflict/theme-monokai"
+import "ace-builds/src-noconflict/theme-terminal"
+import "ace-builds/src-noconflict/theme-twilight"
+//languages 
+import "ace-builds/src-noconflict/mode-javascript"
+import "ace-builds/src-noconflict/mode-css"
+import "ace-builds/src-noconflict/mode-html"
+import "ace-builds/src-noconflict/mode-python"
+import "ace-builds/src-noconflict/mode-java"
+import "ace-builds/src-noconflict/mode-typescript"
+import "ace-builds/src-noconflict/mode-c_cpp"
+import "ace-builds/src-noconflict/mode-csharp"
+
+const CodeEditor = () => {
+  return (
+    <Resizable minHeight={450}
+    minWidth={500}
+    maxWidth={1000}>
+        <div>
+            <AceEditor
+            value='function(){return "helloworld"}' 
+            showGutter={false}
+            theme="monokai"
+            name="UNIQUE_ID_OF_DIV"
+            mode={"javascipt"}
+            fontSize={16}
+            wrapEnabled={true}
+            showPrintMargin={false}
+            highlightActiveLine={false}
+            editorProps={{$blockScrolling:true}}
+            className="ace-editor-container"/>
+        </div>
+    </Resizable>
+  )
+}
+
+export default CodeEditor
